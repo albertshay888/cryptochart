922e23a61ee65cd4f2b34b4b93467c0311266723
Albert Shay <albertshay888@gmail.com>
2021-05-05 20:49:54 -0700

    table added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Table from "./Table";
 
-function App() {
+
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+export default function App() {
+  const [total, setTotal] = useState(0);
+  const [users, setUsers] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  let t = 0
+  useEffect(() => {
+
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        for (let i = 0; i < res.data.length; i++) {
+          t += res.data[i].market_cap
+        }
+        setUsers(res.data);
+        setLoading(false);
+        setTotal(t);
+      });
+  }, []);
+ 
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Rank",
+        accessor: "market_cap_rank"
+      },
+      {
+        Header: "Icon",
+        accessor: "image",
+        Cell: ({ cell: { value } }) => {
+          const url = value
+          return (
+            <>
+            <img 
+              src={url}
+              alt="new"
+              width="50"
+              height="50"
+            />
+            </>
+          )
+       },
+      },
+      {
+        Header: "Name",
+        accessor: "name"
+      },
+      {
+        Header: "Symbol",
+        accessor: "symbol"
+      },
+
+      {
+        Header: "Price",
+        accessor: "current_price",
+        Cell: ({ cell: { value } }) => {
+          const usdPrice = value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+          return (
+            <>
+            {usdPrice}
+            </>
+          )
+      },
+    },
+    {
+      Header: "Price Change % in 24 hours",
+      accessor: "price_change_percentage_24h",
+      Cell: ({ cell: { value } }) => {
+        const newP = value.toFixed(3) + "%";
+          return (
+            <>
+            {newP}
+            </>
+          )
+      },
+    },
+    {
+      Header: "Volume",
+      accessor: "total_volume",
+      Cell: ({ cell: { value } }) => {
+        const commaVolume = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          return (
+            <>
+            {commaVolume}
+            </>
+          )
+      },
+    },
+    {
+      Header: "All-time High ",
+      accessor: "ath",
+      Cell: ({ cell: { value } }) => {
+        const usdP = value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+          return (
+            <>
+            {usdP}
+            </>
+          )
+      },
+    },
+    {
+      Header: "# days since ATH",
+      accessor: "ath_date",
+      Cell: ({ cell: { value } }) => {
+        const athDate = new Date(value).getTime();
+        const today = new Date().getTime()  
+          return (
+             `${Math.round((today - athDate)/(1000*60*60*24))} days`
+          );
+        }
+      },
+      {
+        Header: "Market Cap and % of Total Market Cap",
+        accessor: "market_cap",
+        Cell: ({ cell: { value } }) => {
+       
+          const ptt = (value / t)
+          const p = (ptt*100).toFixed(3)+ "%"
+       
+          return (
+            <>
+             {value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+              <br></br>
+              <br></br> 
+              {p}
+          </>
+            
+          );
+        
+        } 
+      }  
+    ],
+    [] 
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+       <Styles>
+      <Table columns={columns} data={users} loading={loading} total={total} />;
+   
+    </Styles>
+  
   );
 }
-
-export default App;
---
+++ client/src/Table.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+import { useTable, useSortBy} from "react-table";
+
+
+export default function Table({ columns, data, loading, total }) {
+
+
+
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    
+  } = useTable(
+    {
+      columns,
+      data,
+      loading,
+      total
+    },
+    
+  
+    useSortBy
+  );
+ 
+
+  // We don't want to render all 2000 rows for this example, so cap
+  // it at 20 for this use case
+  const firstPageRows = rows.slice(0, 100);
+
+  return (
+    <>
+   
+      <table  className="table" {...getTableProps()}>
+        <thead className= "table__head">
+          {headerGroups.map((headerGroup) => (
+            <tr className="table__row" {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th className= "table__heading"{...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="table__body" {...getTableBodyProps()}>
+          {firstPageRows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr className="table__row" {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td className="table__cell"
+                      {...cell.getCellProps({
+                  
+                      })}
+                    >
+                      {cell.render("Cell")}
+                    
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+        
+    </>
+  )
+}
